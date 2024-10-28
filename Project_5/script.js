80ace5fcb69ac4bd0bb4cdc44237d0ababe3cfe3
Geensia Xiong <geensia@ad.unc.edu>
2024-10-28 13:25:40 -0400

    credits added

--- Project_5/script.js
+++ Project_5/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-        const navLinks = document.querySelector('.nav-links');
+const navLinks = document.querySelector('.nav-links');
 
-        hamburger.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
+hamburger.addEventListener('click', () => { 
+    navLinks.classList.toggle('active');}
+    );
+// Code Explained: (credits to ChatGPT)
+// hamburger.addEventListener waits for a click & when it is clicked, it runs the code below
+// nav.Links.classList.toggle('active') this is adds or "hides" the link
